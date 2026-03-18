b577864005dfdf81dd665f3acc6cf7d44b4d25e4
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-03-18 13:26:51 -0500

    Update ChadaTech_Clock.cpp
    created functions

--- ChadaTech_Clock.cpp
+++ ChadaTech_Clock.cpp
@@ -4,6 +4,24 @@
 
 using namespace std;
 
+void CurrentTime(double hour, double minutes, double seconds) {
+	cout << "*************************" << "   " << "*************************" << endl;
+	cout << "Current time: " << setw(2) << setfill('0') << hour << ":";
+	cout << setw(2) << setfill('0') << minutes << ":";
+	cout << setw(2) << setfill('0') << seconds << endl;
+	cout << "*************************" << "   " << "*************************" << endl;
+}
+
+void Menu() {
+	cout << setfill(' ') << "Please select 1 - 4: " << endl;
+	cout << "*************************" << endl;
+	cout << setw(13) << left << "* 1 - Add One Hour" << setw(7) << right << "*" << endl;
+	cout << setw(13) << left << "* 2 - Add One Minute" << setw(5) << right << "*" << endl;
+	cout << setw(13) << left << "* 3 - Add One Second" << setw(5) << right << "*" << endl;
+	cout << setw(13) << left << "* 4 - Menu" << setw(12) << right << "*" << endl;
+	cout << setw(13) << left << "* 5 - Exit Program" << setw(7) << right << "*" << endl;
+}
+
 int main() {
 	double hour;
 	double minutes;
@@ -25,56 +43,48 @@ int main() {
 	cout << setw(2) << setfill('0') << minutes << ":";
 	cout << setw(2) << setfill('0') << seconds << endl;
 	cout << "*************************" << "   " << "*************************" << endl;
+	
+	while (selection != 5) {
+		if (selection == 4) {
+			cout << setfill(' ') << "Please select 1 - 4: " << endl;
+			cout << "*************************" << endl;
+			cout << setw(13) << left << "* 1 - Add One Hour" << setw(7) << right << "*" << endl;
+			cout << setw(13) << left << "* 2 - Add One Minute" << setw(5) << right << "*" << endl;
+			cout << setw(13) << left << "* 3 - Add One Second" << setw(5) << right << "*" << endl;
+			cout << setw(13) << left << "* 4 - Menu" << setw(12) << right << "*" << endl;
+			cout << setw(13) << left << "* 5 - Exit Program" << setw(7) << right << "*" << endl;
 
-	while (selection == 4) {
-		cout << setfill(' ') << "Please select 1 - 4: " << endl;
-		cout << "*************************" << endl;
-		cout << setw(13) << left << "* 1 - Add One Hour" << setw(7) << right << "*" << endl;
-		cout << setw(13) << left << "* 2 - Add One Minute" << setw(5) << right << "*" << endl;
-		cout << setw(13) << left << "* 3 - Add One Second" << setw(5) << right << "*" << endl;
-		cout << setw(13) << left << "* 4 - Menu" << setw(12) << right << "*" << endl;
-		cout << setw(13) << left << "* 5 - Exit Program" << setw(7) << right << "*" << endl;
+			cin >> selection;
+		}
 
-		cin >> selection;
+		else if (selection == 1) {
+			hour = hour + 1;
+			CurrentTime(hour, minutes, seconds);
+			cout << "Please select 1 - 5 from the menu: " << endl;
+
+			cin >> selection;
+		}
+
+		else if (selection == 2) {
+			minutes = minutes + 1;
+			CurrentTime(hour, minutes, seconds);
+			cout << "Please select 1 - 5 from the menu: " << endl;
+
+			cin >> selection;
+		}
+
+		else if (selection == 3) {
+			seconds = seconds + 1;
+			CurrentTime(hour, minutes, seconds);
+			cout << "Please select 1 - 5 from the menu: " << endl;
+
+			cin >> selection;
+		}
+
+		else if (selection == 5) {
+			cout << "Quitting program" << endl;
+			break;
+		}
 	}
-
-	while (selection == 1) {
-		hour = hour + 1;
-		cout << "*************************" << "   " << "*************************" << endl;
-		cout << "Current time: " << setw(2) << setfill('0') << hour << ":";
-		cout << setw(2) << setfill('0') << minutes << ":";
-		cout << setw(2) << setfill('0') << seconds << endl;
-		cout << endl;
-		cout << "*************************" << "   " << "*************************" << endl;
-		break;
-	}
-
-	while (selection == 2) {
-		minutes = minutes + 1;
-		cout << "*************************" << "   " << "*************************" << endl;
-		cout << "Current time: " << setw(2) << setfill('0') << hour << ":";
-		cout << setw(2) << setfill('0') << minutes << ":";
-		cout << setw(2) << setfill('0') << seconds << endl;
-		cout << endl;
-		cout << "*************************" << "   " << "*************************" << endl;
-		break;
-	}
-
-	while (selection == 3) {
-		seconds == seconds + 1;
-		cout << "*************************" << "   " << "*************************" << endl;
-		cout << "Current time: " << setw(2) << setfill('0') << hour << ":";
-		cout << setw(2) << setfill('0') << minutes << ":";
-		cout << setw(2) << setfill('0') << seconds << endl;
-		cout << endl;
-		cout << "*************************" << "   " << "*************************" << endl;
-		break;
-	}
-
-	while (selection == 5) {
-		cout << "Quitting program" << endl;
-		break;
-	}
-
 	return 0;
 }
