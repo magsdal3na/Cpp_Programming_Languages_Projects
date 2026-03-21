fd4d1b219bd3949ca5ff09fc38ef45c5c6e5b598
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-03-21 09:51:55 -0500

    Update ChadaTech_Clock.cpp
    updated the hour conversion and changed its name. also removed amPmOutput from the CurrentTime() function.

--- ChadaTech_Clock.cpp
+++ ChadaTech_Clock.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int MilitaryHour(double hour) {
+int HourConversion(double hour) {
 	double tempHour = 00;
 	double militaryHour = 00;
 	
@@ -16,18 +16,33 @@ int MilitaryHour(double hour) {
 	return hour;
 }
 
-void CurrentTime(double hour, double minutes, double seconds, char amPm, string amPmOutput) {
-	if ((toupper(amPm) == 'P') && (hour >= 12)) {
-		MilitaryHour(hour);
+void CurrentTime(double hour, double minutes, double seconds, char& amPm) {
+	string amPmOutput;
+	int displayHour;
+
+	if (toupper(amPm) == 'P') {
 		amPmOutput = "PM";
 	}
 	else {
 		amPmOutput = "AM";
 	}
+
+	if (hour == 0) {
+		displayHour = 12;
+	}
+	else if (hour > 12) {
+		displayHour = hour - 12;
+	}
+	else {
+		displayHour = hour;
+	}
+
 	cout << "*************************" << "   " << "*************************" << endl;
+	// 12-hour side of the display (uses displayHour)
 	cout << "*     " << "12-Hour Clock" << setw(6) << "*" << "   " << "*     " << "24-Hour Clock" << setw(6) << "*" << endl;
-	cout << "*      " << setw(2) << setfill('0') << MilitaryHour(hour) << ":" << setw(2) << setfill('0') << minutes << ":";
+	cout << "*      " << setw(2) << setfill('0') << displayHour << ":" << setw(2) << setfill('0') << minutes << ":";
 	cout << setw(2) << setfill('0') << seconds << " " << amPmOutput << "      *" << "   ";
+	//24-hour side of the display (uses hour)
 	cout << "*        " << setw(2) << setfill('0') << hour << ":" << setw(2) << setfill('0') << minutes << ":";
 	cout << setw(2) << setfill('0') << seconds << "       *" << setfill(' ') << endl;
 	cout << "*************************" << "   " << "*************************" << endl;
@@ -39,9 +54,8 @@ void Menu() {
 	cout << setw(13) << left << "* 1 - Add One Hour" << setw(7) << right << "*" << endl;
 	cout << setw(13) << left << "* 2 - Add One Minute" << setw(5) << right << "*" << endl;
 	cout << setw(13) << left << "* 3 - Add One Second" << setw(5) << right << "*" << endl;
-	cout << setw(13) << left << "* 4 - Change AM/PM" << setw(7) << right << "*" << endl;
-	cout << setw(13) << left << "* 5 - Menu" << setw(12) << right << "*" << endl;
-	cout << setw(13) << left << "* 6 - Exit Program" << setw(7) << right << "*" << endl;
+	cout << setw(13) << left << "* 4 - Menu" << setw(12) << right << "*" << endl;
+	cout << setw(13) << left << "* 5 - Exit Program" << setw(7) << right << "*" << endl;
 	cout << "*************************" << endl;
 }
 
@@ -50,7 +64,7 @@ int main() {
 	double hour;
 	double minutes;
 	double seconds;
-	int selection = 5;
+	int selection = 4;
 	char amPm;
 	string amPmOutput;
 
@@ -64,19 +78,13 @@ int main() {
 
 	cin >> amPm;
 
-	CurrentTime(hour, minutes, seconds, amPm, amPmOutput);
+	CurrentTime(hour, minutes, seconds, amPm);
 	
-	while ((selection != 6) && (hour > -1) && (minutes > -1) && (seconds > -1) && (hour < 25) && (minutes < 60) && (seconds < 60)) {
-		if (selection == 5) {
-			Menu();
+	while ((selection != 5) && (hour > -1) && (minutes > -1) && (seconds > -1) && (hour < 25) && (minutes < 60) && (seconds < 60)) {
 
-			cin >> selection;
-		}
-
-		else if (selection == 1) {
+		if (selection == 1) {
 			hour = hour + 1;
-			MilitaryHour(hour);
-			CurrentTime(hour, minutes, seconds, amPm, amPmOutput);
+			CurrentTime(hour, minutes, seconds, amPm);
 			cout << "Please select 5 to show the menu or enter another option: " << endl;
 			cout << ">>";
 
@@ -89,9 +97,9 @@ int main() {
 				minutes = 0;
 				seconds = 0;
 				hour = hour + 1;
-				MilitaryHour(hour);
+				HourConversion(hour);
 			}
-			CurrentTime(hour, minutes, seconds, amPm, amPmOutput);
+			CurrentTime(hour, minutes, seconds, amPm);
 			cout << "Please select 5 to show the menu or enter another option: " << endl;
 			cout << ">>";
 
@@ -102,26 +110,22 @@ int main() {
 			seconds = seconds + 1;
 			if (seconds > 59) {
 				seconds = 0;
-				minutes = 0;
+				minutes = minutes + 1;
+				if (minutes > 59) {
+					minutes = 0;
+				}
 				hour = hour + 1;
-				MilitaryHour(hour);
+				HourConversion(hour);
 			}
-			CurrentTime(hour, minutes, seconds, amPm, amPmOutput);
+			CurrentTime(hour, minutes, seconds, amPm);
 			cout << "Please select 5 to show the menu or enter another option: " << endl;
 			cout << ">>";
 
 			cin >> selection;
 		}
 
-		else if (selection == 4) {
-			if ((amPmOutput == "AM") || (toupper(amPm) == 'A')) {
-				amPmOutput = "PM";
-			}
-			else {
-				amPmOutput = "AM";
-			}
-			CurrentTime(hour, minutes, seconds, amPm, amPmOutput);
-			cout << "Please select 5 to show the menu or enter another option: " << endl;
+		if (selection == 4) {
+			Menu();
 
 			cin >> selection;
 		}
@@ -132,7 +136,7 @@ int main() {
 		}
 	}
 
-	if ((hour < -1) || (minutes, -1) || (seconds > -1) || (hour > 23) || (minutes > 59) || (seconds > 59)) {
+	if ((hour < -1) || (minutes < -1) || (seconds < -1) || (hour > 23) || (minutes > 59) || (seconds > 59)) {
 		cout << "Invalid time. Please run the program again." << endl;
 	}
 
