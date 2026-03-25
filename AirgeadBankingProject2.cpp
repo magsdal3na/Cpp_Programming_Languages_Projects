effc48bb5b5871ebc03ad63fef64b9f9a9a93eb0
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-03-25 13:29:36 -0500

    Update AirgeadBankingProject2.cpp
    created welcome screen and basic input to test

--- AirgeadBankingProject2.cpp
+++ AirgeadBankingProject2.cpp
@@ -4,8 +4,30 @@
 
 using namespace std;
 
+void Test() {
+	cout << "Test block" << endl;
+}
 
+void WelcomeScreen() { //sets up the welcome screen
+	int selection;
+
+	cout << setfill('*') << setw(34) << "" << endl;
+	cout << setfill('*') << setw(12) << "" << " Welcome! " << setw(12) << "" << endl;
+	cout << "To start the program, please enter" << endl << "1. To quit, please enter 2." << endl;
+	cout << ">>";
+
+	cin >> selection;
+}
 
 int main() {
+	int selection = 0;
+
+	WelcomeScreen();
+
+	while (selection == 1) {
+		Test();
+		break;
+	}
+
 	return 0;
 }
