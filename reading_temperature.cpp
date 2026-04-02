7d859139b14f5b51e1f11dd346ab8c48e1635256
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-04-02 13:12:04 -0500

    Update reading_temperature.cpp
    updated notes and finalized before submitting

--- reading_temperature.cpp
+++ reading_temperature.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip> //for formatting decimals
+#include <iomanip> //for formatting decimals after converting
 using namespace std;
 
 int main() {
@@ -8,7 +8,7 @@ int main() {
 	ofstream outFS; //output file stream
 	string city;
 	int tempFahrenheit;
-	double tempCelsius; //double to prevent 0 degree answer
+	double tempCelsius; //double to prevent answer of "0" when dividing
 
 	//file opening attempt for input
 	cout << "Opening file: FahrenheitTemperature.txt" << endl;
@@ -16,10 +16,11 @@ int main() {
 	inFS.open("FahrenheitTemperature.txt");
 	if (!inFS.is_open()) {
 		cout << "Could not open file: FahrenheitTemperature.txt." << endl;
-		return 1; //error
+		return 1; //classifies as an error
 	}
 
 	//opening attempt for output
+	cout << "Opening CelsiusTemperature.txt." << endl;
 	outFS.open("CelsiusTemperature.txt");
 	if (!outFS.is_open()) {
 		cout << "Could not open file CelsiusTemperature.txt." << endl;
@@ -27,13 +28,16 @@ int main() {
 	}
 
 	//reading and printing input to output
+	cout << "Converting temperatures from Fahrenheit to Celsius." << endl;
 	while (inFS >> city >> tempFahrenheit) {
-		//perform conversion
+		//perform conversion of F to C
 		tempCelsius = (tempFahrenheit - 32.0) * (5.0 / 9.0);
 
 		//write to new file
 		outFS << city << " " << fixed << setprecision(2) << tempCelsius << endl;
 	}
+
+	cout << "Values have been updated." << endl << "Please view CelsiusTemperature.txt to see the results." << endl;
 	//closing both files once read
 	inFS.close();
 	outFS.close();
